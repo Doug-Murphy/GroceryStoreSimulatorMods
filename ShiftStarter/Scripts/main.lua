fae0016e913bda0dd1cc6bbcd2f53db25626db2a
Doug Murphy <doug.murphy82@gmail.com>
2024-10-05 21:17:43 -0400

    Add loading debug printing and version information to ShiftStarter.

--- ShiftStarter/Scripts/main.lua
+++ ShiftStarter/Scripts/main.lua
@@ -1,8 +1,11 @@
 ExecuteWithDelay(3000, function ()
+    local version = "1.0.0"
+    print("Loading version " .. version .. " of ShiftStarter.")
 	RegisterHook("/Game/BPs/AI/Employees/BP_EmployeeManager.BP_EmployeeManager_C:PostLoadGame", function(self)
 		local employeeManager = self:get()
-		
+
 		print("Starting bus arrival")
 		employeeManager.IsBusArrivalTime = true
 	end)
+	print("Successfully loaded version " .. version .. " of ShiftStarter.")
 end)
