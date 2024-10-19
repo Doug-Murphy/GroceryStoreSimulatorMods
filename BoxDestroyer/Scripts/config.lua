2c4b7cee1281d38912fd961f884dd02dfdfd3b4d
nieboczek <106301320+nieboczek@users.noreply.github.com>
2024-10-20 00:56:06 +0200

    Fix BoxDestroyer's and FlashMob's configs (#1)
    * Fix FlashMob's config to use the correct type for modifier_keys field
    * Fix BoxDestroyer's config to use the correct type for modifier_keys field

--- BoxDestroyer/Scripts/config.lua
+++ BoxDestroyer/Scripts/config.lua
@@ -1,12 +1,11 @@
 local config = {
     --@description Modifier keys that need to be pressed with the key set below
+    --@type list[ModifierKey]
+    --@default { ModifierKey.CONTROL }
+    modifier_keys = { ModifierKey.CONTROL },
+    --@description Key to delete all empty boxes
     --@type Key
     --@default Key.F7
-    modifier_keys = { ModifierKey.CONTROL },
-
-    --@description Key to delete all empty boxes
-	--@type Key
-	--@default Key.F7
     key = Key.F7
 }
 return config
--- FlashMob/Scripts/config.lua
+++ FlashMob/Scripts/config.lua
@@ -3,15 +3,13 @@ local config = {
     --@type int
     --@default 50
     max_customer_amount = 50,
-
     --@description Modifier keys that need to be pressed with the key set below
-    --@type Key
-    --@default Key.F7
+    --@type list[ModifierKey]
+    --@default { ModifierKey.CONTROL }
     modifier_keys = { ModifierKey.CONTROL },
-
     --@description Key to delete all empty boxes
-	--@type Key
-	--@default Key.F4
+    --@type Key
+    --@default Key.F4
     key = Key.F4
 }
 return config
